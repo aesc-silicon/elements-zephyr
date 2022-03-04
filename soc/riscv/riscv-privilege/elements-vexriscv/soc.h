4d8e5f986ac0316345156c9ca305f72c6ce2bb0b
Daniel Schultz <dnltz@aesc-silicon.de>
2022-03-04 17:16:44 +0100

    soc: riscv: Rename from elements to elements-vexriscv
    Encode used CPU to soc directory to support different version
    of CPUs later. Also this makes it a little bit more clear what
    CPU is used.
    
    Signed-off-by: Daniel Schultz <dnltz@aesc-silicon.de>

--- soc/riscv/riscv-privilege/elements-vexriscv/soc.h
+++ soc/riscv/riscv-privilege/elements-vexriscv/soc.h
@@ -1,5 +1,5 @@
-#ifndef __RISCV32_ELEMENTS_SOC_H_
-#define __RISCV32_ELEMENTS_SOC_H_
+#ifndef __RISCV32_ELEMENTS_VEXRISCV_SOC_H_
+#define __RISCV32_ELEMENTS_VEXRISCV_SOC_H_
 
 #include <soc_common.h>
 
@@ -11,4 +11,4 @@
 #define RISCV_RAM_BASE			DT_REG_ADDR(DT_INST(0, mmio_sram))
 #define RISCV_RAM_SIZE			DT_REG_SIZE(DT_INST(0, mmio_sram))
 
-#endif /* __RISCV32_ELEMENTS_SOC_H_ */
+#endif /* __RISCV32_ELEMENTS_VEXRISCV_SOC_H_ */
