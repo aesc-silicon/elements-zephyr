997fe3fca90147b1538e805f4d45d6616c40a32f
Daniel Schultz <d.schultz@phytec.de>
2024-02-18 10:46:10 +0100

    soc: riscv: elements-vexriscv: Remove RISCV_RAM_* from soc.h
    The RISCV_RAM_{BASE,SIZE} defines are not required anymore and therefore
    can be removed.
    
    Signed-off-by: Daniel Schultz <d.schultz@phytec.de>

--- soc/riscv/riscv-privileged/elements-vexriscv/soc.h
+++ soc/riscv/riscv-privileged/elements-vexriscv/soc.h
@@ -3,8 +3,4 @@
 
 #include <soc_common.h>
 
-/* lib-c hooks required RAM defined variables */
-#define RISCV_RAM_BASE			DT_REG_ADDR(DT_INST(0, mmio_sram))
-#define RISCV_RAM_SIZE			DT_REG_SIZE(DT_INST(0, mmio_sram))
-
 #endif /* __RISCV32_ELEMENTS_VEXRISCV_SOC_H_ */
